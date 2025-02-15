d0f289a9c54174063c6b1c46d95640d090fcd608
shaheemMPM <shaheem.mpm@gmail.com>
2025-02-15 16:18:24 +0530

    Updates ts product function to make a simple volume discount

--- extensions/product-discount-ts/src/run.ts
+++ extensions/product-discount-ts/src/run.ts
@@ -1,21 +1,36 @@
-import type {
-  RunInput,
-  FunctionRunResult
-} from "../generated/api";
-import {
-  DiscountApplicationStrategy,
-} from "../generated/api";
+import type { RunInput, FunctionRunResult } from "../generated/api";
+import { DiscountApplicationStrategy } from "../generated/api";
 
 const EMPTY_DISCOUNT: FunctionRunResult = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
 
-type Configuration = {};
-
 export function run(input: RunInput): FunctionRunResult {
-  const configuration: Configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
-  return EMPTY_DISCOUNT;
-};
+  const targets = input.cart.lines
+    .filter((line) => line.quantity >= 2)
+    .map((line) => ({
+      cartLine: {
+        id: line.id,
+      },
+    }));
+
+  if (!targets.length) {
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
+
+  return {
+    discounts: [
+      {
+        targets,
+        value: {
+          percentage: {
+            value: "10.0",
+          },
+        },
+      },
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
+  };
+}
